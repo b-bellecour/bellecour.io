4f7edaac8fd95777ca7228edbb338e4290c7b0fe
b-bellecour <baptiste@bellecour.io>
2017-11-28 18:57:58 +0900

    front

--- assets/js/loading.js
+++ assets/js/loading.js
@@ -1,17 +1,42 @@
-var $body = $('html');
+'use strict';
 
-var loading = [
-    { elements: $body, properties: { width: '20%' } },
-    { elements: $body, properties: { width: '30%' } },
-    { elements: $body, properties: { width: '50%' } },
-    { elements: $body, properties: { width: '100%' } },
-    { elements: $body, properties: { height: '100%' }, options: { 
-      complete: function () { 
-        $('.wrap').velocity( 'transition.slideUpIn' );
-        $('img').velocity( 'transition.flipYIn' );
-        $('html').css({ background: '#fff' });
-      }
-    }
+function getRandomArbitrary(min, max) {
+  return Math.random() * (max - min) + min;
+}
+
+// super simple router - go to page specified in hash, otherwise go to "default"
+function router(route) {
+  var pageName = route ? route : $('.default.page').attr('data-page-name');
+  var $page = $('[data-page-name="' + pageName + '"]');
+  $('.page').css('display', 'none');
+  $('[data-page]').removeClass('active');
+  $('[data-page="' + pageName + '"]').addClass('active');
+  $page.css('display', 'block');
+}
+router();
+
+// fake loader
+var progress = 0;
+var fakeLoaderInterval = window.setInterval(function () {
+  var $lp = $('.loading-progress');
+  progress = progress + getRandomArbitrary(10, 25);
+  $lp.css('transform', 'translateX(' + progress + '%)');
+
+  if (progress >= 75) {
+    window.clearInterval(fakeLoaderInterval);
+    $lp.css('transform', 'translateX(100%)');
+    setTimeout(function () {
+      return $('.loading').css('transform', 'translateY(calc(100% + 10px))');
+    }, 400);
   }
-]; 
-$.Velocity.RunSequence(loading);
+}, getRandomArbitrary(100, 500));
+
+// navigation
+$('.main-nav li a').on('click', function (e) {
+  var $this = $(e.currentTarget);
+  var route = $this.attr('data-page');
+
+  $('.main-nav li a').removeClass('active');
+  $this.addClass('active');
+  router(route);
+});
