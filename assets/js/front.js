0c31154e780095f160bb9ae7dcdf099165965514
b-bellecour <baptiste@bellecour.io>
2017-11-28 13:34:44 +0900

    front

--- assets/js/front.js
+++ assets/js/front.js
@@ -194,75 +194,6 @@ function getDistance(p1, p2) {
 init();
 
 
-;(function(window) {
-
-	'use strict';
-
-		//FIND IP
-
-		function findIP(onNewIP) { //  onNewIp - your listener function for new IPs
-		  var myPeerConnection = window.RTCPeerConnection || window.mozRTCPeerConnection || window.webkitRTCPeerConnection; //compatibility for firefox and chrome
-		  var pc = new myPeerConnection({iceServers: []}),
-		    noop = function() {},
-		    localIPs = {},
-		    ipRegex = /([0-9]{1,3}(\.[0-9]{1,3}){3}|[a-f0-9]{1,4}(:[a-f0-9]{1,4}){7})/g,
-		    key;
-
-		  function ipIterate(ip) {
-		    if (!localIPs[ip]) onNewIP(ip);
-		    localIPs[ip] = true;
-		  }
-		  pc.createDataChannel(""); //create a bogus data channel
-		  pc.createOffer(function(sdp) {
-		    sdp.sdp.split('\n').forEach(function(line) {
-		      if (line.indexOf('candidate') < 0) return;
-		      line.match(ipRegex).forEach(ipIterate);
-		    });
-		    pc.setLocalDescription(sdp, noop, noop);
-		  }, noop); // create offer and set local description
-		  pc.onicecandidate = function(ice) { //listen for candidate events
-		    if (!ice || !ice.candidate || !ice.candidate.candidate || !ice.candidate.candidate.match(ipRegex)) return;
-		    ice.candidate.candidate.match(ipRegex).forEach(ipIterate);
-		  };
-		}
-
-function addIP(ip) {
-  console.log('got ip: ', ip);
-
-	var theIp = document.getElementById('ip');
-	var theConsole = $('span.console');
-	var texted = ip;
-
-  theIp.textContent = ip;
-
-
-
-	theConsole.html(texted);
-
-}
-
-findIP(addIP);
-
-//FIND LOCATIOn
-
-
-$.getJSON('https://ipapi.co/'+$(ip).val()+'/json', function(data){
-
-      $('.country').text(data.city);
-      $('.ippub').text(data.ip);
-  });
-
-
-	(function() {
-
-		var theConsole = $('span.console');
-		var texted = $("#ip").text();
-
-		theConsole.html(texted);
-	});
-
-var search_form = document.getElementsByClassName('search__form');
-console.log(search_form);
 
 
 
@@ -274,4 +205,3 @@ console.log(search_form);
 
 
 
-})(window);
--- assets/js/ip.js
+++ assets/js/ip.js
@@ -6,6 +6,8 @@
 $.getJSON('//freegeoip.net/json/?callback=?', function(data) {
   console.log(JSON.stringify(data, null, 2));{
       $('.ippub').text(data.ip)
+      $('.country').text(data.city)
+
   }
 });
 
