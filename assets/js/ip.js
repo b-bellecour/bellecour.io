2d54a9476a8dada9020895fd5fbfb354201ae8b3
b-bellecour <baptiste@bellecour.io>
2017-12-05 11:30:15 +0900

    front

--- assets/js/ip.js
+++ assets/js/ip.js
@@ -3,7 +3,7 @@
 //FIND LOCATIOn
 
 
-$.getJSON('//freegeoip.net/json/?callback=?', function(data) {
+$.getJSON('https://freegeoip.net/json/?callback=?', function(data) {
   console.log(JSON.stringify(data, null, 2));{
       $('.ippub').text(data.ip)
       $('.country').text(data.city)
